58434212a31df2375d5d27264c21099e37db5e14
DeividsonGonini <deividsongonini@gmail.com>
2020-07-29 22:40:49 -0300

    Aula 3 Formulario

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,16 +1,133 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
 //Criado pagina como Componente
 function CadastroCategoria() {
+
+
+    const valoresIniciais = {
+        nome: '',
+        descricao: '',
+        cor: '',
+    }
+
+    //criando State para guardar as Categorias criadas
+    const [categorias, setCategorias] = useState([]);
+
+    //criando o State para guardar o texto das categorias
+    //nomeDaCategoria = nome que estamos dando para a Categoria/  setNomedaCategoria= função para alterar / useState('Filmes') = Nome inicial 
+    const [values, setValues] = useState(valoresIniciais);
+
+
+
+
+    function setValue(chave, valor) {
+        //chava: valor variavel, porde ser nome, descricao, blablabla
+        setValues({
+            ...values,
+            [chave]: valor, //nome: 'valor'
+        })
+    }
+    //função para capturar as mudanças
+    function handleChange(infosDoEvento) {
+        // const { getAttribute, value } = infosDoEvento.target;
+        setValue(
+
+            infosDoEvento.target.getAttribute('name'),
+            infosDoEvento.target.value
+        );
+    }
+
+
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+            <h1>Cadastro de Categoria: {values.nome}</h1>
+
+            {/* onSubmit -> para nao submeter da forma padrao os inputs mantendo o conceito de SPA */}
+            <form onSubmit={function handleSubmit(infosDoEvento) {
+                // previne que seja inserido o valor Default
+                infosDoEvento.preventDefault();
+                //setando a Categoria
+                setCategorias([
+                    //... serve para manter tudo o que ja tem na lista e apenas acrescentar o novo / categorias é o nome da categoria que setamos na função CadastroCategoria
+                    ...categorias,
+                    values
+                ]);
+
+                setValues(valoresIniciais)
+            }}>
+
+                {/* Pegando o nome pelo FormFiel (componente criado) */}
+                <FormField
+                    label="Nome"
+                    type="text"
+                    name="nome"
+                    value={values.nome}
+                    onChange={handleChange}
+                />
+
+
+                <FormField
+                    label="Descrição"
+                    name="descricao"
+                    type="text"
+                    value={values.descricao}
+                    onChange={handleChange}
+                />
+
+                {/* <div>
+                    <label>
+                        Descrição:
+                        <textarea
+                            value={values.descricao}
+                            onChange={handleChange}
+                        />
+                    </label>
+                </div> */}
+
+                <FormField
+                    label="Cor"
+                    name="cor"
+                    type="color"
+                    value={values.cor}
+                    onChange={handleChange}
+                />
+
+
+                {/* <div>
+
+                    <label>
+                        Cor:
+                        <input
+                            value={values.cor}
+                            onChange={handleChange}
+                        />
+                    </label>
+                </div> */}
+
+
+                <button>
+                    Cadastrar
+                    </button>
+            </form>
+
+            <ul>
+                {/* indice utilizado para poder duplicar os dados */}
+                {categorias.map((categorias, indice) => {
+                    return (
+                        <li key={`${categorias}${indice}`}>
+                            {categorias.nome}
+                        </li>
+                    )
+                })}
+            </ul>
+
             <Link to="/">
                 Ir para Home
             </Link>
-        </PageDefault>
+        </PageDefault >
     )
 }
 
